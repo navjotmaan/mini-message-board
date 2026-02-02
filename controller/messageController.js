cfb6e4f1f34633774919f7c02703c8d397c77355
navjotmaan <navjotmaan137@gmail.com>
2026-02-02 15:50:10 +0000

    add validation for form input

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,10 +1,21 @@
+const { validationResult } = require('express-validator');
 const db = require('../db/queries');
 
 exports.getForm = (req, res) => {
-    res.render('form', { title: 'Mini Messageboard | New'});
+    res.render('form', { title: 'Mini Messageboard | New', errors: [], data: {} });
 };
 
 exports.postMessage = async (req, res) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.status(400).render('form', {
+            title: 'Mini Messageboard | New',
+            errors: errors.array(),
+            data: req.body 
+        });
+    }
+
     try {
         await db.insertMessage(req.body.name, req.body.message);
         res.redirect('/');
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,9 +1,17 @@
 const { Router } = require('express');
 const messageRouter = Router();
+const { body } = require('express-validator');
 
 const messageController = require('../controller/messageController');
 
+const validateMessage = [
+  body('name').trim()
+    .isLength({ min: 1, max: 20 }).withMessage('Name must be between 1 and 20 characters'),
+  body('message').trim()
+    .isLength({ min: 1, max: 200 }).withMessage('Message must be between 1 and 200 characters')
+];
+
 messageRouter.get('/', messageController.getForm);
-messageRouter.post('/', messageController.postMessage);
+messageRouter.post('/', validateMessage, messageController.postMessage);
 
 module.exports = messageRouter;
