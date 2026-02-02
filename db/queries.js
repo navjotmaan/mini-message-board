5e17868e875a2efcb609658057948444b5609b5b
navjotmaan <navjotmaan137@gmail.com>
2026-02-02 15:20:41 +0000

    add query to get message by id

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,12 @@ async function insertMessage(username, text) {
     await pool.query("INSERT INTO messages (username, text, added) VALUES ($1, $2, NOW())", [username, text]);
 }
 
+async function getMessageById(id) {
+    return await pool.query("SELECT * FROM messages WHERE id = $1", [id]);
+}
+
 module.exports = { 
     getAllMessages,
     insertMessage,
+    getMessageById
 };
