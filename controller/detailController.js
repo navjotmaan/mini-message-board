7badbab4d518cc8a1827336e744f861165e4e435
navjotmaan <navjotmaan137@gmail.com>
2026-02-02 15:22:15 +0000

    change accordingly to use data from database

--- controller/detailController.js
+++ controller/detailController.js
@@ -1,12 +1,23 @@
-const messages = require('../data/messages');
+const db = require('../db/queries');
 
-exports.showDetail = (req, res) => {
+exports.showDetail = async (req, res) => {
     const id = req.params.id;
-    const message = messages[id]; 
     
-    if (message) {
-        res.render('message', { title: 'Mini messageboard | message', message }); 
-    } else {
-        res.status(404).send('Message not found');
+    try {
+        const result = await db.getMessageById(id);
+        const message = result.rows[0];
+
+        if (message) {
+            res.render('message', { 
+                title: 'Mini messageboard | message', 
+                message: message 
+            }); 
+        } else {
+            res.status(404).send('Message not found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
     }
+    
 };
