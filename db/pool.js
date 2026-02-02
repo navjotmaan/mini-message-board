1802e239ad99c883fad1318f2173a4dab69518ed
navjotmaan <navjotmaan137@gmail.com>
2026-02-02 10:38:24 +0000

    fix pool.js

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-});
+const isProduction = process.env.DATABASE_URL;
+
+module.exports = new Pool(
+  isProduction 
+    ? { 
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false } 
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
