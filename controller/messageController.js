78cda97ee6c6a4828b8ea13509235dd37b632e4f
navjotmaan <navjotmaan137@gmail.com>
2026-02-02 11:13:24 +0000

    write async function to insert message

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,17 +1,15 @@
-const messages = require('../data/messages');
+const db = require('../db/queries');
 
 exports.getForm = (req, res) => {
     res.render('form', { title: 'Mini Messageboard | New'});
 };
 
-exports.postMessage = (req, res) => {
-    const { name, message } = req.body;
-
-    messages.push({ 
-        text: message, 
-        user: name, 
-        added: new Date().toLocaleString()
-    });
-
-    res.redirect('/');
+exports.postMessage = async (req, res) => {
+    try {
+        await db.insertMessage(req.body.name, req.body.message);
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error saving message");
+    }
 };
